1615a37de7879337b1e5de92aea21d75d9d19c9e
pelldav <pelldav@gmail.com>
2013-08-30 15:47:05 +0200

    Dump db added

---
+++ Dump DB/San Vito/db San Vito Measure.sql
@@ -0,0 +1,351 @@
+INSERT INTO `db_auth`.`Measure` (`ID`,`COMPANY`,`NOISELOGGER`,`TIMESTAMP`,`VALUE`,`BATTERY`)
+VALUES ('1','SMAT','1','2013-22-07 14:30','0','98');
+
+INSERT INTO `db_auth`.`Measure` (`ID`,`COMPANY`,`NOISELOGGER`,`TIMESTAMP`,`VALUE`,`BATTERY`)
+values ('2','SMAT','2','2013-22-07 14:30','0','98');
+
+INSERT INTO `db_auth`.`Measure` (`ID`,`COMPANY`,`NOISELOGGER`,`TIMESTAMP`,`VALUE`,`BATTERY`) 
+values ('3','SMAT','3','2013-22-07 14:30','0','98');
+
+INSERT INTO `db_auth`.`Measure` (`ID`,`COMPANY`,`NOISELOGGER`,`TIMESTAMP`,`VALUE`,`BATTERY`) 
+values ('4','SMAT','4','2013-22-07 14:30','0','98');
+
+INSERT INTO `db_auth`.`Measure` (`ID`,`COMPANY`,`NOISELOGGER`,`TIMESTAMP`,`VALUE`,`BATTERY`) 
+values ('5','SMAT','5','2013-22-07 14:30','0','98');
+
+INSERT INTO `db_auth`.`Measure` (`ID`,`COMPANY`,`NOISELOGGER`,`TIMESTAMP`,`VALUE`,`BATTERY`) 
+values ('6','SMAT','6','2013-22-07 14:30','0','98');
+
+INSERT INTO `db_auth`.`Measure` (`ID`,`COMPANY`,`NOISELOGGER`,`TIMESTAMP`,`VALUE`,`BATTERY`) 
+values ('7','SMAT','7','2013-22-07 14:30','0','98');
+
+INSERT INTO `db_auth`.`Measure` (`ID`,`COMPANY`,`NOISELOGGER`,`TIMESTAMP`,`VALUE`,`BATTERY`) 
+values ('8','SMAT','8','2013-22-07 14:30','0','98');
+
+INSERT INTO `db_auth`.`Measure` (`ID`,`COMPANY`,`NOISELOGGER`,`TIMESTAMP`,`VALUE`,`BATTERY`) 
+values ('9','SMAT','9','2013-22-07 14:30','0','98');
+
+INSERT INTO `db_auth`.`Measure` (`ID`,`COMPANY`,`NOISELOGGER`,`TIMESTAMP`,`VALUE`,`BATTERY`) 
+values ('10','SMAT','10','2013-22-07 14:30','0','98');
+
+INSERT INTO `db_auth`.`Measure` (`ID`,`COMPANY`,`NOISELOGGER`,`TIMESTAMP`,`VALUE`,`BATTERY`) 
+values ('11','SMAT','11','2013-22-07 14:30','0','98');
+
+INSERT INTO `db_auth`.`Measure` (`ID`,`COMPANY`,`NOISELOGGER`,`TIMESTAMP`,`VALUE`,`BATTERY`) 
+values ('12','SMAT','12','2013-22-07 14:30','0','98');
+
+INSERT INTO `db_auth`.`Measure` (`ID`,`COMPANY`,`NOISELOGGER`,`TIMESTAMP`,`VALUE`,`BATTERY`) 
+values ('13','SMAT','13','2013-22-07 14:30','0','98');
+
+INSERT INTO `db_auth`.`Measure` (`ID`,`COMPANY`,`NOISELOGGER`,`TIMESTAMP`,`VALUE`,`BATTERY`) 
+values ('14','SMAT','14','2013-22-07 14:30','0','98');
+
+INSERT INTO `db_auth`.`Measure` (`ID`,`COMPANY`,`NOISELOGGER`,`TIMESTAMP`,`VALUE`,`BATTERY`) 
+values ('15','SMAT','15','2013-22-07 14:30','0','98');
+
+INSERT INTO `db_auth`.`Measure` (`ID`,`COMPANY`,`NOISELOGGER`,`TIMESTAMP`,`VALUE`,`BATTERY`) 
+values ('16','SMAT','16','2013-22-07 14:30','0','98');
+
+INSERT INTO `db_auth`.`Measure` (`ID`,`COMPANY`,`NOISELOGGER`,`TIMESTAMP`,`VALUE`,`BATTERY`) 
+values ('17','SMAT','17','2013-22-07 14:30','0','98');
+
+INSERT INTO `db_auth`.`Measure` (`ID`,`COMPANY`,`NOISELOGGER`,`TIMESTAMP`,`VALUE`,`BATTERY`) 
+values ('18','SMAT','18','2013-22-07 14:30','0','98');
+
+INSERT INTO `db_auth`.`Measure` (`ID`,`COMPANY`,`NOISELOGGER`,`TIMESTAMP`,`VALUE`,`BATTERY`) 
+values ('19','SMAT','19','2013-22-07 14:30','0','98');
+
+INSERT INTO `db_auth`.`Measure` (`ID`,`COMPANY`,`NOISELOGGER`,`TIMESTAMP`,`VALUE`,`BATTERY`) 
+values ('20','SMAT','20','2013-22-07 14:30','0','98');
+
+INSERT INTO `db_auth`.`Measure` (`ID`,`COMPANY`,`NOISELOGGER`,`TIMESTAMP`,`VALUE`,`BATTERY`) 
+values ('21','SMAT','21','2013-22-07 14:30','0','98');
+
+INSERT INTO `db_auth`.`Measure` (`ID`,`COMPANY`,`NOISELOGGER`,`TIMESTAMP`,`VALUE`,`BATTERY`) 
+values ('22','SMAT','22','2013-22-07 14:30','0','98');
+
+INSERT INTO `db_auth`.`Measure` (`ID`,`COMPANY`,`NOISELOGGER`,`TIMESTAMP`,`VALUE`,`BATTERY`) 
+values ('23','SMAT','23','2013-22-07 14:30','0','98');
+
+INSERT INTO `db_auth`.`Measure` (`ID`,`COMPANY`,`NOISELOGGER`,`TIMESTAMP`,`VALUE`,`BATTERY`) 
+values ('24','SMAT','24','2013-22-07 14:30','0','98');
+
+INSERT INTO `db_auth`.`Measure` (`ID`,`COMPANY`,`NOISELOGGER`,`TIMESTAMP`,`VALUE`,`BATTERY`) 
+values ('25','SMAT','25','2013-22-07 14:30','0','98');
+
+INSERT INTO `db_auth`.`Measure` (`ID`,`COMPANY`,`NOISELOGGER`,`TIMESTAMP`,`VALUE`,`BATTERY`) 
+values ('26','SMAT','26','2013-22-07 14:30','0','98');
+
+INSERT INTO `db_auth`.`Measure` (`ID`,`COMPANY`,`NOISELOGGER`,`TIMESTAMP`,`VALUE`,`BATTERY`) 
+values ('27','SMAT','27','2013-22-07 14:30','0','98');
+
+INSERT INTO `db_auth`.`Measure` (`ID`,`COMPANY`,`NOISELOGGER`,`TIMESTAMP`,`VALUE`,`BATTERY`) 
+values ('28','SMAT','28','2013-22-07 14:30','0','98');
+
+INSERT INTO `db_auth`.`Measure` (`ID`,`COMPANY`,`NOISELOGGER`,`TIMESTAMP`,`VALUE`,`BATTERY`) 
+values ('29','SMAT','29','2013-22-07 14:30','0','98');
+
+INSERT INTO `db_auth`.`Measure` (`ID`,`COMPANY`,`NOISELOGGER`,`TIMESTAMP`,`VALUE`,`BATTERY`) 
+values ('30','SMAT','30','2013-22-07 14:30','0','98');
+
+INSERT INTO `db_auth`.`Measure` (`ID`,`COMPANY`,`NOISELOGGER`,`TIMESTAMP`,`VALUE`,`BATTERY`) 
+values ('31','SMAT','31','2013-22-07 14:30','0','98');
+
+INSERT INTO `db_auth`.`Measure` (`ID`,`COMPANY`,`NOISELOGGER`,`TIMESTAMP`,`VALUE`,`BATTERY`) 
+values ('32','SMAT','32','2013-22-07 14:30','0','98');
+
+INSERT INTO `db_auth`.`Measure` (`ID`,`COMPANY`,`NOISELOGGER`,`TIMESTAMP`,`VALUE`,`BATTERY`) 
+values ('33','SMAT','33','2013-22-07 14:30','0','98');
+
+INSERT INTO `db_auth`.`Measure` (`ID`,`COMPANY`,`NOISELOGGER`,`TIMESTAMP`,`VALUE`,`BATTERY`) 
+values ('34','SMAT','34','2013-22-07 14:30','0','98');
+
+INSERT INTO `db_auth`.`Measure` (`ID`,`COMPANY`,`NOISELOGGER`,`TIMESTAMP`,`VALUE`,`BATTERY`) 
+values ('35','SMAT','35','2013-22-07 14:30','0','98');
+
+INSERT INTO `db_auth`.`Measure` (`ID`,`COMPANY`,`NOISELOGGER`,`TIMESTAMP`,`VALUE`,`BATTERY`) 
+values ('36','SMAT','36','2013-22-07 14:30','0','98');
+
+INSERT INTO `db_auth`.`Measure` (`ID`,`COMPANY`,`NOISELOGGER`,`TIMESTAMP`,`VALUE`,`BATTERY`) 
+values ('37','SMAT','37','2013-22-07 14:30','0','98');
+
+INSERT INTO `db_auth`.`Measure` (`ID`,`COMPANY`,`NOISELOGGER`,`TIMESTAMP`,`VALUE`,`BATTERY`) 
+values ('38','SMAT','38','2013-22-07 14:30','0','98');
+
+INSERT INTO `db_auth`.`Measure` (`ID`,`COMPANY`,`NOISELOGGER`,`TIMESTAMP`,`VALUE`,`BATTERY`) 
+values ('39','SMAT','39','2013-22-07 14:30','0','98');
+
+INSERT INTO `db_auth`.`Measure` (`ID`,`COMPANY`,`NOISELOGGER`,`TIMESTAMP`,`VALUE`,`BATTERY`) 
+values ('40','SMAT','40','2013-22-07 14:30','0','98');
+
+INSERT INTO `db_auth`.`Measure` (`ID`,`COMPANY`,`NOISELOGGER`,`TIMESTAMP`,`VALUE`,`BATTERY`) 
+values ('41','SMAT','41','2013-22-07 14:30','0','98');
+
+INSERT INTO `db_auth`.`Measure` (`ID`,`COMPANY`,`NOISELOGGER`,`TIMESTAMP`,`VALUE`,`BATTERY`) 
+values ('42','SMAT','42','2013-22-07 14:30','0','98');
+
+INSERT INTO `db_auth`.`Measure` (`ID`,`COMPANY`,`NOISELOGGER`,`TIMESTAMP`,`VALUE`,`BATTERY`) 
+values ('43','SMAT','43','2013-22-07 14:30','0','98');
+
+INSERT INTO `db_auth`.`Measure` (`ID`,`COMPANY`,`NOISELOGGER`,`TIMESTAMP`,`VALUE`,`BATTERY`) 
+values ('44','SMAT','44','2013-22-07 14:30','0','98');
+
+INSERT INTO `db_auth`.`Measure` (`ID`,`COMPANY`,`NOISELOGGER`,`TIMESTAMP`,`VALUE`,`BATTERY`) 
+values ('45','SMAT','45','2013-22-07 14:30','0','98');
+
+INSERT INTO `db_auth`.`Measure` (`ID`,`COMPANY`,`NOISELOGGER`,`TIMESTAMP`,`VALUE`,`BATTERY`) 
+values ('46','SMAT','46','2013-22-07 14:30','0','98');
+
+INSERT INTO `db_auth`.`Measure` (`ID`,`COMPANY`,`NOISELOGGER`,`TIMESTAMP`,`VALUE`,`BATTERY`) 
+values ('47','SMAT','47','2013-22-07 14:30','0','98');
+
+INSERT INTO `db_auth`.`Measure` (`ID`,`COMPANY`,`NOISELOGGER`,`TIMESTAMP`,`VALUE`,`BATTERY`) 
+values ('48','SMAT','48','2013-22-07 14:30','0','98');
+
+INSERT INTO `db_auth`.`Measure` (`ID`,`COMPANY`,`NOISELOGGER`,`TIMESTAMP`,`VALUE`,`BATTERY`) 
+values ('49','SMAT','49','2013-22-07 14:30','0','98');
+
+INSERT INTO `db_auth`.`Measure` (`ID`,`COMPANY`,`NOISELOGGER`,`TIMESTAMP`,`VALUE`,`BATTERY`) 
+values ('50','SMAT','50','2013-22-07 14:30','0','98');
+
+INSERT INTO `db_auth`.`Measure` (`ID`,`COMPANY`,`NOISELOGGER`,`TIMESTAMP`,`VALUE`,`BATTERY`) 
+values ('51','SMAT','51','2013-22-07 14:30','0','98');
+
+INSERT INTO `db_auth`.`Measure` (`ID`,`COMPANY`,`NOISELOGGER`,`TIMESTAMP`,`VALUE`,`BATTERY`) 
+values ('52','SMAT','52','2013-22-07 14:30','0','98');
+
+INSERT INTO `db_auth`.`Measure` (`ID`,`COMPANY`,`NOISELOGGER`,`TIMESTAMP`,`VALUE`,`BATTERY`) 
+values ('53','SMAT','53','2013-22-07 14:30','0','98');
+
+INSERT INTO `db_auth`.`Measure` (`ID`,`COMPANY`,`NOISELOGGER`,`TIMESTAMP`,`VALUE`,`BATTERY`) 
+values ('54','SMAT','54','2013-22-07 14:30','0','98');
+
+INSERT INTO `db_auth`.`Measure` (`ID`,`COMPANY`,`NOISELOGGER`,`TIMESTAMP`,`VALUE`,`BATTERY`) 
+values ('55','SMAT','55','2013-22-07 14:30','0','98');
+
+INSERT INTO `db_auth`.`Measure` (`ID`,`COMPANY`,`NOISELOGGER`,`TIMESTAMP`,`VALUE`,`BATTERY`) 
+values ('56','SMAT','56','2013-22-07 14:30','0','98');INSERT INTO `db_auth`.`Measure` (`ID`,`COMPANY`,`NOISELOGGER`,`TIMESTAMP`,`VALUE`,`BATTERY`)
+VALUES ('57','SMAT','57','2013-22-07 14:30','0','98');
+
+INSERT INTO `db_auth`.`Measure` (`ID`,`COMPANY`,`NOISELOGGER`,`TIMESTAMP`,`VALUE`,`BATTERY`)
+values ('58','SMAT','58','2013-22-07 14:30','0','98');
+
+INSERT INTO `db_auth`.`Measure` (`ID`,`COMPANY`,`NOISELOGGER`,`TIMESTAMP`,`VALUE`,`BATTERY`) 
+values ('59','SMAT','59','2013-22-07 14:30','0','98');
+
+INSERT INTO `db_auth`.`Measure` (`ID`,`COMPANY`,`NOISELOGGER`,`TIMESTAMP`,`VALUE`,`BATTERY`) 
+values ('60','SMAT','60','2013-22-07 14:30','0','98');
+
+INSERT INTO `db_auth`.`Measure` (`ID`,`COMPANY`,`NOISELOGGER`,`TIMESTAMP`,`VALUE`,`BATTERY`) 
+values ('61','SMAT','61','2013-22-07 14:30','0','98');
+
+INSERT INTO `db_auth`.`Measure` (`ID`,`COMPANY`,`NOISELOGGER`,`TIMESTAMP`,`VALUE`,`BATTERY`) 
+values ('62','SMAT','62','2013-22-07 14:30','0','98');
+
+INSERT INTO `db_auth`.`Measure` (`ID`,`COMPANY`,`NOISELOGGER`,`TIMESTAMP`,`VALUE`,`BATTERY`) 
+values ('63','SMAT','63','2013-22-07 14:30','0','98');
+
+INSERT INTO `db_auth`.`Measure` (`ID`,`COMPANY`,`NOISELOGGER`,`TIMESTAMP`,`VALUE`,`BATTERY`) 
+values ('64','SMAT','64','2013-22-07 14:30','0','98');
+
+INSERT INTO `db_auth`.`Measure` (`ID`,`COMPANY`,`NOISELOGGER`,`TIMESTAMP`,`VALUE`,`BATTERY`) 
+values ('65','SMAT','65','2013-22-07 14:30','0','98');
+
+INSERT INTO `db_auth`.`Measure` (`ID`,`COMPANY`,`NOISELOGGER`,`TIMESTAMP`,`VALUE`,`BATTERY`) 
+values ('66','SMAT','66','2013-22-07 14:30','0','98');
+
+INSERT INTO `db_auth`.`Measure` (`ID`,`COMPANY`,`NOISELOGGER`,`TIMESTAMP`,`VALUE`,`BATTERY`) 
+values ('67','SMAT','67','2013-22-07 14:30','0','98');
+
+INSERT INTO `db_auth`.`Measure` (`ID`,`COMPANY`,`NOISELOGGER`,`TIMESTAMP`,`VALUE`,`BATTERY`) 
+values ('68','SMAT','68','2013-22-07 14:30','0','98');
+
+INSERT INTO `db_auth`.`Measure` (`ID`,`COMPANY`,`NOISELOGGER`,`TIMESTAMP`,`VALUE`,`BATTERY`) 
+values ('69','SMAT','69','2013-22-07 14:30','0','98');
+
+INSERT INTO `db_auth`.`Measure` (`ID`,`COMPANY`,`NOISELOGGER`,`TIMESTAMP`,`VALUE`,`BATTERY`) 
+values ('70','SMAT','70','2013-22-07 14:30','0','98');
+
+INSERT INTO `db_auth`.`Measure` (`ID`,`COMPANY`,`NOISELOGGER`,`TIMESTAMP`,`VALUE`,`BATTERY`) 
+values ('71','SMAT','71','2013-22-07 14:30','0','98');
+
+INSERT INTO `db_auth`.`Measure` (`ID`,`COMPANY`,`NOISELOGGER`,`TIMESTAMP`,`VALUE`,`BATTERY`) 
+values ('72','SMAT','72','2013-22-07 14:30','0','98');
+
+INSERT INTO `db_auth`.`Measure` (`ID`,`COMPANY`,`NOISELOGGER`,`TIMESTAMP`,`VALUE`,`BATTERY`) 
+values ('73','SMAT','73','2013-22-07 14:30','0','98');
+
+INSERT INTO `db_auth`.`Measure` (`ID`,`COMPANY`,`NOISELOGGER`,`TIMESTAMP`,`VALUE`,`BATTERY`) 
+values ('74','SMAT','74','2013-22-07 14:30','0','98');
+
+INSERT INTO `db_auth`.`Measure` (`ID`,`COMPANY`,`NOISELOGGER`,`TIMESTAMP`,`VALUE`,`BATTERY`) 
+values ('75','SMAT','75','2013-22-07 14:30','0','98');
+
+INSERT INTO `db_auth`.`Measure` (`ID`,`COMPANY`,`NOISELOGGER`,`TIMESTAMP`,`VALUE`,`BATTERY`) 
+values ('76','SMAT','76','2013-22-07 14:30','0','98');
+
+INSERT INTO `db_auth`.`Measure` (`ID`,`COMPANY`,`NOISELOGGER`,`TIMESTAMP`,`VALUE`,`BATTERY`) 
+values ('77','SMAT','77','2013-22-07 14:30','0','98');
+
+INSERT INTO `db_auth`.`Measure` (`ID`,`COMPANY`,`NOISELOGGER`,`TIMESTAMP`,`VALUE`,`BATTERY`) 
+values ('78','SMAT','78','2013-22-07 14:30','0','98');
+
+INSERT INTO `db_auth`.`Measure` (`ID`,`COMPANY`,`NOISELOGGER`,`TIMESTAMP`,`VALUE`,`BATTERY`) 
+values ('79','SMAT','79','2013-22-07 14:30','0','98');
+
+INSERT INTO `db_auth`.`Measure` (`ID`,`COMPANY`,`NOISELOGGER`,`TIMESTAMP`,`VALUE`,`BATTERY`) 
+values ('80','SMAT','80','2013-22-07 14:30','0','98');
+
+INSERT INTO `db_auth`.`Measure` (`ID`,`COMPANY`,`NOISELOGGER`,`TIMESTAMP`,`VALUE`,`BATTERY`) 
+values ('81','SMAT','81','2013-22-07 14:30','0','98');
+
+INSERT INTO `db_auth`.`Measure` (`ID`,`COMPANY`,`NOISELOGGER`,`TIMESTAMP`,`VALUE`,`BATTERY`) 
+values ('82','SMAT','82','2013-22-07 14:30','0','98');
+
+INSERT INTO `db_auth`.`Measure` (`ID`,`COMPANY`,`NOISELOGGER`,`TIMESTAMP`,`VALUE`,`BATTERY`) 
+values ('83','SMAT','83','2013-22-07 14:30','0','98');
+
+INSERT INTO `db_auth`.`Measure` (`ID`,`COMPANY`,`NOISELOGGER`,`TIMESTAMP`,`VALUE`,`BATTERY`) 
+values ('84','SMAT','84','2013-22-07 14:30','0','98');
+
+INSERT INTO `db_auth`.`Measure` (`ID`,`COMPANY`,`NOISELOGGER`,`TIMESTAMP`,`VALUE`,`BATTERY`) 
+values ('85','SMAT','85','2013-22-07 14:30','0','98');
+
+INSERT INTO `db_auth`.`Measure` (`ID`,`COMPANY`,`NOISELOGGER`,`TIMESTAMP`,`VALUE`,`BATTERY`) 
+values ('86','SMAT','86','2013-22-07 14:30','0','98');
+
+INSERT INTO `db_auth`.`Measure` (`ID`,`COMPANY`,`NOISELOGGER`,`TIMESTAMP`,`VALUE`,`BATTERY`) 
+values ('87','SMAT','87','2013-22-07 14:30','0','98');
+
+INSERT INTO `db_auth`.`Measure` (`ID`,`COMPANY`,`NOISELOGGER`,`TIMESTAMP`,`VALUE`,`BATTERY`) 
+values ('88','SMAT','88','2013-22-07 14:30','0','98');
+
+INSERT INTO `db_auth`.`Measure` (`ID`,`COMPANY`,`NOISELOGGER`,`TIMESTAMP`,`VALUE`,`BATTERY`) 
+values ('89','SMAT','89','2013-22-07 14:30','0','98');
+
+INSERT INTO `db_auth`.`Measure` (`ID`,`COMPANY`,`NOISELOGGER`,`TIMESTAMP`,`VALUE`,`BATTERY`) 
+values ('90','SMAT','90','2013-22-07 14:30','0','98');
+
+INSERT INTO `db_auth`.`Measure` (`ID`,`COMPANY`,`NOISELOGGER`,`TIMESTAMP`,`VALUE`,`BATTERY`) 
+values ('91','SMAT','91','2013-22-07 14:30','0','98');
+
+INSERT INTO `db_auth`.`Measure` (`ID`,`COMPANY`,`NOISELOGGER`,`TIMESTAMP`,`VALUE`,`BATTERY`) 
+values ('92','SMAT','92','2013-22-07 14:30','0','98');
+
+INSERT INTO `db_auth`.`Measure` (`ID`,`COMPANY`,`NOISELOGGER`,`TIMESTAMP`,`VALUE`,`BATTERY`) 
+values ('93','SMAT','93','2013-22-07 14:30','0','98');
+
+INSERT INTO `db_auth`.`Measure` (`ID`,`COMPANY`,`NOISELOGGER`,`TIMESTAMP`,`VALUE`,`BATTERY`) 
+values ('94','SMAT','94','2013-22-07 14:30','0','98');
+
+INSERT INTO `db_auth`.`Measure` (`ID`,`COMPANY`,`NOISELOGGER`,`TIMESTAMP`,`VALUE`,`BATTERY`) 
+values ('95','SMAT','95','2013-22-07 14:30','0','98');
+
+INSERT INTO `db_auth`.`Measure` (`ID`,`COMPANY`,`NOISELOGGER`,`TIMESTAMP`,`VALUE`,`BATTERY`) 
+values ('96','SMAT','96','2013-22-07 14:30','0','98');
+
+INSERT INTO `db_auth`.`Measure` (`ID`,`COMPANY`,`NOISELOGGER`,`TIMESTAMP`,`VALUE`,`BATTERY`) 
+values ('97','SMAT','97','2013-22-07 14:30','0','98');
+
+INSERT INTO `db_auth`.`Measure` (`ID`,`COMPANY`,`NOISELOGGER`,`TIMESTAMP`,`VALUE`,`BATTERY`) 
+values ('98','SMAT','98','2013-22-07 14:30','0','98');
+
+INSERT INTO `db_auth`.`Measure` (`ID`,`COMPANY`,`NOISELOGGER`,`TIMESTAMP`,`VALUE`,`BATTERY`) 
+values ('99','SMAT','99','2013-22-07 14:30','0','98');
+
+INSERT INTO `db_auth`.`Measure` (`ID`,`COMPANY`,`NOISELOGGER`,`TIMESTAMP`,`VALUE`,`BATTERY`) 
+values ('100','SMAT','100','2013-22-07 14:30','0','98');
+
+INSERT INTO `db_auth`.`Measure` (`ID`,`COMPANY`,`NOISELOGGER`,`TIMESTAMP`,`VALUE`,`BATTERY`) 
+values ('101','SMAT','101','2013-22-07 14:30','0','98');
+
+INSERT INTO `db_auth`.`Measure` (`ID`,`COMPANY`,`NOISELOGGER`,`TIMESTAMP`,`VALUE`,`BATTERY`) 
+values ('102','SMAT','102','2013-22-07 14:30','0','98');
+
+INSERT INTO `db_auth`.`Measure` (`ID`,`COMPANY`,`NOISELOGGER`,`TIMESTAMP`,`VALUE`,`BATTERY`) 
+values ('103','SMAT','103','2013-22-07 14:30','0','98');
+
+INSERT INTO `db_auth`.`Measure` (`ID`,`COMPANY`,`NOISELOGGER`,`TIMESTAMP`,`VALUE`,`BATTERY`) 
+values ('104','SMAT','104','2013-22-07 14:30','0','98');
+
+INSERT INTO `db_auth`.`Measure` (`ID`,`COMPANY`,`NOISELOGGER`,`TIMESTAMP`,`VALUE`,`BATTERY`) 
+values ('105','SMAT','105','2013-22-07 14:30','0','98');
+
+INSERT INTO `db_auth`.`Measure` (`ID`,`COMPANY`,`NOISELOGGER`,`TIMESTAMP`,`VALUE`,`BATTERY`) 
+values ('106','SMAT','106','2013-22-07 14:30','0','98');
+
+INSERT INTO `db_auth`.`Measure` (`ID`,`COMPANY`,`NOISELOGGER`,`TIMESTAMP`,`VALUE`,`BATTERY`) 
+values ('107','SMAT','107','2013-22-07 14:30','0','98');
+
+INSERT INTO `db_auth`.`Measure` (`ID`,`COMPANY`,`NOISELOGGER`,`TIMESTAMP`,`VALUE`,`BATTERY`) 
+values ('108','SMAT','108','2013-22-07 14:30','0','98');
+
+INSERT INTO `db_auth`.`Measure` (`ID`,`COMPANY`,`NOISELOGGER`,`TIMESTAMP`,`VALUE`,`BATTERY`) 
+values ('109','SMAT','109','2013-22-07 14:30','0','98');
+
+INSERT INTO `db_auth`.`Measure` (`ID`,`COMPANY`,`NOISELOGGER`,`TIMESTAMP`,`VALUE`,`BATTERY`) 
+values ('110','SMAT','110','2013-22-07 14:30','0','98');
+
+INSERT INTO `db_auth`.`Measure` (`ID`,`COMPANY`,`NOISELOGGER`,`TIMESTAMP`,`VALUE`,`BATTERY`) 
+values ('111','SMAT','111','2013-22-07 14:30','0','98');
+
+INSERT INTO `db_auth`.`Measure` (`ID`,`COMPANY`,`NOISELOGGER`,`TIMESTAMP`,`VALUE`,`BATTERY`) 
+values ('112','SMAT','112','2013-22-07 14:30','0','98');
+
+INSERT INTO `db_auth`.`Measure` (`ID`,`COMPANY`,`NOISELOGGER`,`TIMESTAMP`,`VALUE`,`BATTERY`) 
+values ('113','SMAT','113','2013-22-07 14:30','0','98');
+
+INSERT INTO `db_auth`.`Measure` (`ID`,`COMPANY`,`NOISELOGGER`,`TIMESTAMP`,`VALUE`,`BATTERY`) 
+values ('114','SMAT','114','2013-22-07 14:30','0','98');
+
+INSERT INTO `db_auth`.`Measure` (`ID`,`COMPANY`,`NOISELOGGER`,`TIMESTAMP`,`VALUE`,`BATTERY`) 
+values ('115','SMAT','115','2013-22-07 14:30','0','98');
+
+INSERT INTO `db_auth`.`Measure` (`ID`,`COMPANY`,`NOISELOGGER`,`TIMESTAMP`,`VALUE`,`BATTERY`) 
+values ('116','SMAT','116','2013-22-07 14:30','0','98');
+
+INSERT INTO `db_auth`.`Measure` (`ID`,`COMPANY`,`NOISELOGGER`,`TIMESTAMP`,`VALUE`,`BATTERY`) 
+values ('117','SMAT','117','2013-22-07 14:30','0','98');
+
+INSERT INTO `db_auth`.`Measure` (`ID`,`COMPANY`,`NOISELOGGER`,`TIMESTAMP`,`VALUE`,`BATTERY`) 
+values ('118','SMAT','118','2013-22-07 14:30','0','98');
